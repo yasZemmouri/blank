35a9f09d2cde42efe8367a947d6a3c669d6b69bd
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2023-12-27 06:45:15 -0500

    index template with language dynamic variable

---
+++ pages/index.php
@@ -0,0 +1,17 @@
+<?php
+    $language = 'en';
+?>
+<!DOCTYPE html>
+<html lang="<?=$language?>">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    hello world
+</body>
+
+</html>
