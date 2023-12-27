deed020defe50475835b935dc1cd180a02631621
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2023-12-27 07:46:23 -0500

    title and metas

--- includes/config.php
+++ includes/config.php
@@ -2,5 +2,7 @@
 //This file is for dependency links and global variables.
 //Language
 $language = 'en';
+//Website Name
+$siteName="Site Name"
     
 ?>
--- pages/index.php
+++ pages/index.php
@@ -1,13 +1,20 @@
 <?php
     include '../includes/config.php';
+    // snippet variable 
+    $title = "page title";
 ?>
 <!DOCTYPE html>
 <html lang="<?=$language?>">
 
 <head>
+    <!-- title -->
+    <title><?= $siteName . " - " . $title; ?></title>
+    <!-- Meta -->
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="description"
+        content="Google is now saying that the maximum length for a snippet in search results is 320 characters including spaces & snippet features, like the Published Date. I more often recommend a length of 300 characters for a Meta Description">
 </head>
 
 <body>
